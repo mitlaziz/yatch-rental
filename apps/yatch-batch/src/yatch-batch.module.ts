8f0c85a7711faa542010fd022a374fa9657d5bba
Max <lazizyuldoshev93@gmail.com>
2024-07-04 16:06:14 +0900

    feat: configure environmental variables

--- apps/yatch-batch/src/yatch-batch.module.ts
+++ apps/yatch-batch/src/yatch-batch.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { YatchBatchController } from './yatch-batch.controller';
 import { YatchBatchService } from './yatch-batch.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [YatchBatchController],
   providers: [YatchBatchService],
 })
