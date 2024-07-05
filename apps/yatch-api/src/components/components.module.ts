3b6d9d023b2628fc7f80440c0442003669f9690d
Max <lazizyuldoshev93@gmail.com>
2024-07-05 21:03:18 +0900

    feat: create components related modules

--- apps/yatch-api/src/components/components.module.ts
+++ apps/yatch-api/src/components/components.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { MemberModule } from './member/member.module';
 import { PropertyModule } from './property/property.module';
+import { AuthModule } from './auth/auth.module';
+import { BoardArticleModule } from './board-article/board-article.module';
+import { CommentModule } from './comment/comment.module';
+import { FollowModule } from './follow/follow.module';
+import { LikeModule } from './like/like.module';
+import { ViewModule } from './view/view.module';
 
 @Module({
-  imports: [MemberModule, PropertyModule]
+	imports: [
+		MemberModule,
+		PropertyModule,
+		AuthModule,
+		BoardArticleModule,
+		CommentModule,
+		FollowModule,
+		LikeModule,
+		ViewModule,
+	],
 })
 export class ComponentsModule {}
