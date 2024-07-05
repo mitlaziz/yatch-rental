849abb8b7d6d3902e8bb542452de42243ddac8bb
Max <lazizyuldoshev93@gmail.com>
2024-07-05 21:51:37 +0900

    feat: create member resolver & service model

---
+++ apps/yatch-api/src/components/member/member.resolver.ts
@@ -0,0 +1,31 @@
+import { Resolver, Mutation, Query } from '@nestjs/graphql';
+import { MemberService } from './member.service';
+
+@Resolver()
+export class MemberResolver {
+	constructor(private readonly memberService: MemberService) {}
+
+	@Mutation(() => String)
+	public async signup(): Promise<string> {
+		console.log('Mutation: signup');
+		return this.memberService.signup();
+	}
+
+	@Mutation(() => String)
+	public async login(): Promise<string> {
+		console.log('Mutation: login');
+		return 'login executed!';
+	}
+
+	@Mutation(() => String)
+	public async updateMember(): Promise<string> {
+		console.log('Mutation: updateMember');
+		return 'updateMember executed!';
+	}
+
+	@Query(() => String)
+	public async getMember(): Promise<string> {
+		console.log('Query: getMember');
+		return 'getMember executed!';
+	}
+}
