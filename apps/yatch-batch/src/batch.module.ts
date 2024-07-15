051d2baa5a4b71744023b9eed1c141890c6560f9
Max <lazizyuldoshev93@gmail.com>
2024-07-15 13:46:48 +0900

    fix: modify job scheduler business logic

--- apps/yatch-batch/src/batch.module.ts
+++ apps/yatch-batch/src/batch.module.ts
@@ -4,9 +4,18 @@ import { BatchService } from './batch.service';
 import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 import { DatabaseModule } from './database/database.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import PropertySchema from 'apps/yatch-api/src/schemas/Property.model';
+import MemberSchema from 'apps/yatch-api/src/schemas/Member.model';
 
 @Module({
-	imports: [ConfigModule.forRoot(), DatabaseModule, ScheduleModule.forRoot()],
+	imports: [
+		ConfigModule.forRoot(),
+		DatabaseModule,
+		ScheduleModule.forRoot(),
+		MongooseModule.forFeature([{ name: 'Property', schema: PropertySchema }]),
+		MongooseModule.forFeature([{ name: 'Member', schema: MemberSchema }]),
+	],
 	controllers: [BatchController],
 	providers: [BatchService],
 })
